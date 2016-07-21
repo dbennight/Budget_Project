7c6b6fe4a895bb37ad3efe4dcf7501b45c7c8c83
bro <brody.prestwich666@gmail.com>
2016-07-21 17:51:50 -0600

    bros update

--- src/Budget_Project.java
+++ src/Budget_Project.java
@@ -2,4 +2,15 @@
  * Created by david bennight on 7/21/2016.
  */
 public class Budget_Project {
+
+    public static void main(String args[]){
+
+        System.out.println("stuff");
+
+
+
+
+    }
+
+
 }
