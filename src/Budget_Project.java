563a69325ce59ae752b855b312d32e66a6cf6fe7
dbennight <dbennight1983@hotmail.com>
2016-07-21 18:42:19 -0600

    I wrote you a little something

--- src/Budget_Project.java
+++ src/Budget_Project.java
@@ -12,35 +12,12 @@ public class Budget_Project {
 
         for(int i = 0; i < 1000000; i++){
 
-            System.out.println("david loves the cock!");
+            System.out.println("brody loves the cack!");
 
         }
 
 
     }
-
-
-    dsfkjfdslfdaskj;lfdakjfw;lkwje;lrewjwrelkrewjrewlkrewjlrew
-
-    f
-            fdasfd
-                    fdasfdgh
-    h
-            d
-    public h getD() {
-        return .getString(R.string.fsfdfdsd)
-                ghhgdf
-
-                        gfd
-                        gds
-                                h
-                                toString()thher
-
-
-    }
-
-    das;jfdsjksfdjdgf
-    jfdsjksfdjdgf
 
 
 }
