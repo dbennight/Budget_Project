835a4504608f008868abf585ed564ec4f7e6d571
bro <brody.prestwich666@gmail.com>
2016-07-21 19:44:11 -0600

    made it work

--- src/Budget_Project.java
+++ src/Budget_Project.java
@@ -1,30 +1,50 @@
+import java.io.IOException;
+import java.util.Scanner;
+
 /**
  * Created by david bennight on 7/21/2016.
  */
 public class Budget_Project {
 
-    public static void main(String args[]) {
+
+
+
+    public static void main(String args[]) throws IOException {
+
+        double stuff;
+        double stuff2;
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        stuff = in.nextDouble();
+        System.out.print("Enter a another number: ");
+        stuff2  = in.nextDouble();
+
+        if (SalaryChecker(stuff, stuff2)) {
+            System.out.println("its true an works");
+
+
+        }
 
 
     }
+
     // come up with better name for salary checker
-    public boolean SalaryChecker(double salary, double bills){
+    public static boolean SalaryChecker(double salary, double bills) {
 
-        if(salary >= bills){
+        if (salary >= bills) {
             return true;
-        } else{
-            return false;}
+        } else {
+            return false;
+
+        }
 
 
     }
 
 
-
-
-
-
-
-    }
+}
 
 
 
