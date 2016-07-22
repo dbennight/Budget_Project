6b2cfdba788343bd08952ffe27ae15e4e9ca6119
bro <brody.prestwich666@gmail.com>
2016-07-21 18:42:38 -0600

    fixed

--- src/Budget_Project.java
+++ src/Budget_Project.java
@@ -3,14 +3,14 @@
  */
 public class Budget_Project {
 
-    public static void main(String args[]){
+    public static void main(String args[]) {
 
         System.out.println("stuff");
         System.out.print("david sucks");
         System.out.print("david sucks");
         System.out.print("david sucks");
 
-        for(int i = 0; i < 1000000; i++){
+        for (int i = 0; i < 1000000; i++) {
 
             System.out.println("david loves the cock!");
 
@@ -18,29 +18,5 @@ public class Budget_Project {
 
 
     }
-
-
-    dsfkjfdslfdaskj;lfdakjfw;lkwje;lrewjwrelkrewjrewlkrewjlrew
-
-    f
-            fdasfd
-                    fdasfdgh
-    h
-            d
-    public h getD() {
-        return .getString(R.string.fsfdfdsd)
-                ghhgdf
-
-                        gfd
-                        gds
-                                h
-                                toString()thher
-
-
-    }
-
-    das;jfdsjksfdjdgf
-    jfdsjksfdjdgf
-
 
 }
